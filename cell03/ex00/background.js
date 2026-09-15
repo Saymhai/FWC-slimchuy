6083ec1243abe2dd8cab466a07e8d328e486b7c7
Saymhai <151415377+Saymhai@users.noreply.github.com>
2026-09-16 01:05:47 +0700

    7

---
+++ cell03/ex00/background.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('btn')
+
+function changeBackground() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256)
+
+    const randomColor = `rgb(${r}, ${g}, ${b})`;
+
+    return randomColor
+}
+
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = changeBackground();
+});
---
+++ cell03/ex02/calc.js
@@ -0,0 +1,8 @@
+const lValue = document.getElementById('leftOper');
+const rValue = document.getElementById('rightOper');
+const operator = document.getElementById('operator');
+const btn = document.getElementById('btn');
+
+function calcurate () {
+    
+}
